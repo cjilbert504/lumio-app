536bf671b32ca1bcab69ba983711c1442ebb567e
Collin Jilbert <cjilbert504@gmail.com>
2026-03-26 18:29:27 -0500

    comment out distDir

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: "foobar",
+  // distDir: "foobar",
 };
 
 export default nextConfig;
